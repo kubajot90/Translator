cb107fcfd1517dee5732c5aa4d702fdf65fb0f9a
Jakub Jakubowski <kubajot6@wp.pl>
2022-08-05 12:35:26 +0100

    Add hide panel by buttons & items, change display of buttons after language chose.

--- js/LanguagePanel.js
+++ js/LanguagePanel.js
@@ -3,22 +3,29 @@ import { languagesList } from "./languagesList.js";
 export class LanguagePanel {
   constructor() {
     this.languagePanelElm = null;
-    this.languageButtonsElm = null;
+    this.languageExpandButtonsElm = null;
     this.mainPanelElm = null;
     this.mainFooterElm = null;
     this.footerElm = null;
     this.expandIconsBackgroundElm = null;
     this.languagesListElm = null;
     this.languageListItems = null;
+    this.languageButtonsLeft = null;
+    this.languageButtonsRight = null;
 
     this.isbuttonExpandclick = false;
+
+    this.buttonLanguages = ["pl", "en", "de"];
+    this.currentLanguage = this.buttonLanguages[0];
+    this.lastLanguage = this.buttonLanguages[1];
+    this.beforeLastLanguage = this.buttonLanguages[2];
   }
 
   init() {
     this.createListItem();
     this.htmlElements();
-
     this.eventlisteners();
+    this.buttonText();
   }
 
   htmlElements() {
@@ -30,14 +37,22 @@ export class LanguagePanel {
     this.expandIconsBackgroundElm = document.querySelectorAll(
       "[data-expand-icon-background]"
     );
-    this.languageButtonsElm = document.querySelectorAll(
+    this.languageExpandButtonsElm = document.querySelectorAll(
       "[data-button-lang-expand]"
     );
     this.languageListItems = document.querySelectorAll("[data-language-item]");
+
+    this.languageButtonsLeft = document.querySelectorAll(
+      "[data-buttons-lang-left]"
+    );
+
+    this.languageButtonsRight = document.querySelectorAll(
+      "[data-buttons-lang-right]"
+    );
   }
 
   eventlisteners() {
-    this.languageButtonsElm.forEach((button, index) =>
+    this.languageExpandButtonsElm.forEach((button, index) =>
       button.addEventListener("click", () => {
         this.togglePanelVisibility();
         this.buttonsAnimation(button, index);
@@ -47,9 +62,59 @@ export class LanguagePanel {
     this.languageListItems.forEach((item) => {
       item.addEventListener("click", (e) => {
         console.log(e.target.dataset.code);
+        const languageCode = e.target.dataset.code;
+        // const languageName = languagesList[languageCode].name;
         this.activeItem(item);
+        this.changeLanguage(languageCode);
+        this.buttonText();
+        this.togglePanelVisibility();
+        this.isbuttonExpandclick = false;
+        this.rotateButtonsBack();
       });
     });
+
+    this.languageButtonsLeft.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        console.log(e.target.dataset.code);
+        if (this.isbuttonExpandclick) {
+          this.isbuttonExpandclick = false;
+          this.togglePanelVisibility();
+          this.rotateButtonsBack();
+        }
+      });
+    });
+  }
+
+  // changeLanguage(languageName) {
+  //   const temporary = this.buttonLanguages[0];
+  //   this.buttonLanguages[0] = languageName;
+  //   this.buttonLanguages[2] = this.buttonLanguages[1];
+  //   this.buttonLanguages[1] = temporary;
+
+  //   this.setButtonLanguagesOrder();
+  //   console.log(this.buttonLanguages);
+  // }
+
+  // setButtonLanguagesOrder() {
+  //   this.currentLanguage = this.buttonLanguages[0];
+  //   this.lastLanguage = this.buttonLanguages[1];
+  //   this.beforeLastLanguage = this.buttonLanguages[2];
+  // }
+
+  changeLanguage(languageCode) {
+    const temporary = this.buttonLanguages[0];
+    this.buttonLanguages[0] = languageCode;
+    this.buttonLanguages[2] = this.buttonLanguages[1];
+    this.buttonLanguages[1] = temporary;
+
+    this.setButtonLanguagesOrder();
+    console.log(this.buttonLanguages);
+  }
+
+  setButtonLanguagesOrder() {
+    this.currentLanguage = this.buttonLanguages[0];
+    this.lastLanguage = this.buttonLanguages[1];
+    this.beforeLastLanguage = this.buttonLanguages[2];
   }
 
   togglePanelVisibility() {
@@ -63,11 +128,25 @@ export class LanguagePanel {
     element.classList.toggle("hide");
   }
 
+  // buttonText() {
+  //   this.languageButtonsLeft.forEach((button, index) => {
+  //     button.textContent = this.buttonLanguages[index].toUpperCase();
+  //   });
+  // }
+  buttonText() {
+    this.languageButtonsLeft.forEach((button, index) => {
+      button.textContent =
+        languagesList[this.buttonLanguages[index]].name.toUpperCase();
+      button.dataset.code = this.buttonLanguages[index];
+    });
+  }
+
   buttonsAnimation(button, index) {
     if (this.isbuttonExpandclick) {
-      this.languageButtonsElm.forEach((element) =>
-        element.classList.remove("rotate")
-      );
+      // this.languageExpandButtonsElm.forEach((element) =>
+      //   element.classList.remove("rotate")
+      // );
+      this.rotateButtonsBack();
       this.isbuttonExpandclick = false;
     } else {
       button.classList.toggle("rotate");
@@ -77,6 +156,10 @@ export class LanguagePanel {
     const backgroudElm = this.expandIconsBackgroundElm[index];
     backgroudElm.classList.add("expand-icon--click");
     setTimeout(() => backgroudElm.classList.remove("expand-icon--click"), 300);
+  }
+
+  rotateButtonsBack(button = this.languageExpandButtonsElm) {
+    button.forEach((element) => element.classList.remove("rotate"));
   }
 
   createListItem() {
