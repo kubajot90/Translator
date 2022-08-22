43ffd21082d54b5effb0318f29dbb141d54fabd7
Jakub Jakubowski <kubajot6@wp.pl>
2022-08-22 19:01:21 +0100

    Finish style save panel

--- js/FooterIcons.js
+++ js/FooterIcons.js
@@ -12,6 +12,30 @@ export class FooterIcons {
   init() {
     this.htmlElements();
     this.eventlisteners();
+
+    // clickAnimation(ClassSelector){
+    //   ClassSelector = document.querySelector(".options__sort");
+    //   ClassSelector.addEventListener("click", () => {
+    //     const style = document.createElement("style");
+    //     style.innerText = `.options__sort::after {
+    //     position: absolute;
+    //     content:'';
+    //     top: 0;
+    //     left: 0;
+    //     width: 100%;
+    //     height: 100%;
+    //     border-radius: 4px;
+    //     background-color:  #5f636818;
+    //     transition: .4s;
+    //     animation: clickAnimation .1s ;
+    // }`;
+    //     ClassSelector.appendChild(style);
+    //     setTimeout(() => {
+    //       style.innerText = "";
+    //     }, 100);
+    //     ClassSelector.appendChild(style);
+    //   });
+    // }
   }
 
   htmlElements() {
@@ -31,25 +55,25 @@ export class FooterIcons {
   eventlisteners() {
     this.historyButtonElm.addEventListener("click", () => {
       this.toggleHistoryPanel();
-      if (this.savePanelElm.classList.contains("save__panel--show"))
+      if (this.savePanelElm.classList.contains("panel--show"))
         this.toggleSavePanel();
     });
 
     this.saveButtonElm.addEventListener("click", () => {
       this.toggleSavePanel();
-      if (this.historyPanelElm.classList.contains("history__panel--show"))
+      if (this.historyPanelElm.classList.contains("panel--show"))
         this.toggleHistoryPanel();
     });
   }
 
   toggleHistoryPanel() {
-    this.historyPanelElm.classList.toggle("history__panel--show");
+    this.historyPanelElm.classList.toggle("panel--show");
     this.historyButtonElm.classList.toggle("footer__icon--active");
     this.historySubtitleElm.classList.toggle("footer__icon-subtitle--active");
   }
 
   toggleSavePanel() {
-    this.savePanelElm.classList.toggle("save__panel--show");
+    this.savePanelElm.classList.toggle("panel--show");
     this.saveButtonElm.classList.toggle("footer__icon--active");
     this.saveSubtitleElm.classList.toggle("footer__icon-subtitle--active");
   }
