650ab20dce02a5402d719078eb562f356af01335
kubajot90 <kubajot90@gmail.com>
2023-01-20 17:01:04 +0100

    Add API request test

--- cypress/e2e/UI-tests.cy.js
+++ cypress/e2e/UI-tests.cy.js
@@ -4,12 +4,23 @@ describe ("E2E - home page", () => {
         cy.get("[data-popup-button]").click();
       })
 
-    it ("Expand language menu and select language", () => {
-        cy.get("[data-button-lang-expand]").each((button)=>{
-            cy.wrap(button).click();
-            cy.get("[data-lang-list]>li").each((li)=>{
-                cy.wrap(li).click({ force: true });
-            })
+      it ("API request test", () => {
+        cy.intercept("GET","https://api.mymemory.translated.net/*").as("translateRequest");
+        // cy.get("[data-textarea-left]").type("test");
+        cy.wait("@translateRequest");
+        cy.get("@translateRequest").then(res => {
+          expect(res.response.statusCode).to.equal(200)
         })
-        })  
+      })
+
+    // it ("Expand language menu and select language", () => {
+    //     cy.get("[data-button-lang-expand]").each((button)=>{
+    //         cy.wrap(button).click();
+    //         cy.get("[data-lang-list]>li").each((li)=>{
+    //             cy.wrap(li).click({ force: true });
+    //         })
+    //     })
+    //     })  
+
+
 })
